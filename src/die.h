87529e62c1b6abf34d3fa5cdb54eed9b19f908cd
amarino25 <amanda.marino2@g.austincc.edu>
2022-12-04 16:31:05 -0600

    testing

--- src/die.h
+++ src/die.h
@@ -1,1 +1,34 @@
 //
+#include <string>
+#include <vector>
+#include<iostream>
+
+#ifndef DIE_H
+#define DIE_H
+
+class die
+{
+    public:
+    void roll();
+    int rolled_value()const;
+
+    private:
+    int roll_value();
+    int sides{6};
+
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+#endif
