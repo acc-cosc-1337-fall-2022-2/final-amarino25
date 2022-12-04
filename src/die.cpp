b7cc99821fd1efcdf17e4a1a42348b8bedb62904
amarino25 <amanda.marino2@g.austincc.edu>
2022-12-04 16:32:15 -0600

    testing

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,25 @@
 //
+#include "die.h"
+#include<iostream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::cin;
+
+void die::roll()
+{
+
+}
+int die::rolled_value()const
+{
+
+}
+
+int die::roll_value()
+{
+
+}
+
